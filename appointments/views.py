eaa8e8265f3a68338e25def2a8e936b41430c601
denisceno <dceno88@gmail.com>
2025-03-10 23:33:52 +0100

    Added Swal

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import AppointmentForm
 from .models import Appointment
+from django.contrib import messages
 import smtplib
 
 import os
@@ -8,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-email_txt = "Thank you for scheduling an appointment with us!"
+
 my_email = os.getenv('MY_EMAIL')
 my_password = os.getenv('MY_PASSWORD')
 
@@ -27,21 +28,27 @@ def email_s(user_email, email_txt):
         print(f"Error sending email: {e}")
 
 
+from django.contrib import messages  # Import messages framework
+
 def appointment_home(request):
     template_name = "appointments/appointments-home.html"
+
     if request.method == "POST":
         form = AppointmentForm(request.POST)
         if form.is_valid():
+            
             appointment = form.save()
+            messages.success(request, "Your appointment has been scheduled successfully!")  # Add success message
+            name = form.cleaned_data.get('name')
+            date = form.cleaned_data.get('appointment_date')
+            email_txt = f"Thank you {name} for scheduling an appointment with us! \n\nYour appointment is scheduled for {date}. \n\nWe look forward to seeing you soon!"
             email_s(appointment.email, email_txt)
-            return redirect("home-page")
+            return redirect("home-page")  # Redirect to prevent form resubmission
     else:
         form = AppointmentForm()
 
-    context = {
-        "form": form,
-    }
-    return render(request, template_name, context)
+    return render(request, template_name, {"form": form})
+
 
 def appointment_list(request):
     template_name = 'appointments/appointment_list.html'
