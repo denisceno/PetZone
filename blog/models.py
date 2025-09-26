78fe6afaef09fc9c4529b580e2613fd7af6841eb
denisceno <dceno88@gmail.com>
2025-09-26 10:41:45 +0200

    fixes

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='blog_images/', null=True, blank=True) 
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True, default='images/no_image_available.png') 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
