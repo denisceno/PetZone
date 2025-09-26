a5101ab07dd6b5df7da125fdd8a4e48c5e3f3052
denisceno <dceno88@gmail.com>
2025-09-26 12:00:46 +0200

    fixes

--- PetZone/settings.py
+++ PetZone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
